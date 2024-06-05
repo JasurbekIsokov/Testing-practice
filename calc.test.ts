a28d96a649f3e2f0bbaad06a2a7212c2290c3512
Jasurbek <jasurisoqov685@gmail.com>
2024-06-05 12:18:03 +0300

    Configurartion global

--- calc.test.ts
+++ calc.test.ts
@@ -1,9 +1,11 @@
-import { describe, expect, it } from "vitest";
+// import { describe, expect, it } from "vitest";
+import { Calc } from "./calc";
 
 describe("calc", () => {
   it("should return 2 for 1 + 1");
   const actual = 1 + 1;
   const describe = 2;
+  const calc = new Calc();
 
   expect(actual).toBe(describe); // ===;
 });
