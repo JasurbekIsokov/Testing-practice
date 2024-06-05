e18f396884cd2dc592faf20da45d888a14d4b380
Jasurbek <jasurisoqov685@gmail.com>
2024-06-06 00:06:19 +0300

    Tooling (Prettier, ESLint, TypeScript, Husky, Lint-staged)

--- calc.test.ts
+++ calc.test.ts
@@ -1,8 +1,8 @@
 // import { describe, expect, it } from "vitest";
-import { Calc } from "./calc";
+import { Calc } from './calc';
 
-describe("calc", () => {
-  it("should return 2 for 1 + 1");
+describe('calc', () => {
+  it('should return 2 for 1 + 1');
   const actual = 1 + 1;
   const describe = 2;
   const calc = new Calc();
